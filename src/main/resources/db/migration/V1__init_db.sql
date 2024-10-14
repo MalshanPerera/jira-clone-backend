908de9e126d17879a883c6fb92eb0c534d8794ae
MalshanPerera <malshanperera3@gmail.com>
2024-10-14 12:04:51 +0530

    Fix: user sql fixed

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
@@ -7,3 +9,4 @@ CREATE TABLE user (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
